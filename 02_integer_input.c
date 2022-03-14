f8cd351a252101600ca5819242a55f9d035fcfba
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-14 17:26:55 +0530

    Add files via upload

---
+++ 02_integer_input.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0;
+    printf("\n Enter a number:");
+    scanf("%d",&i);
+    printf("\n____________");
+
+    printf("\n your number is: %i",i);
+    printf("\n____________");
+
+    getch();
+    return 0;
+}
