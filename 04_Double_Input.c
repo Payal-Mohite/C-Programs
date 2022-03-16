16f376f5aa45a24f99774c5addb6da7c429952c0
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-16 14:33:18 +0530

    Add files via upload

---
+++ 04_Double_Input.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+#include<conio.h>
+int main()
+{
+    double d=0.0;
+
+    printf("\n Enter your number: ");
+    scanf("%lf",&d);
+
+    printf("------------*****-------------");
+
+    printf("\n\t \"Your number is:%lf \"\n",d);
+    printf("\n thank you");
+
+    printf("\n------------*****-------------");
+
+    getch();
+    return 0;
+}
