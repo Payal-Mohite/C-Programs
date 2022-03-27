585ee873e7dd530d92e9398847a9fa0b6d48c730
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-27 17:54:23 +0530

    Add files via upload

---
+++ 05_Float_Input.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float f=50.3;
+    printf("\n Enter a number: ");
+    scanf("%f",&f);
+    printf("\n ---------*******-----------------");
+    printf("\n Your number is:%0.3f ",f);
+    printf("\n Thank you");
+
+    printf("\n ---------*******-----------------");
+
+    getch();
+    return 0;
+
+}
