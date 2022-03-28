3d7a296cfdc6aca1cfb4632b10471f86ba142afc
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-28 23:08:02 +0530

    Add files via upload

---
+++ 06_Static_Int_Data.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=900;
+
+    printf("\n \t Given Number is:%i",i);
+
+    printf("\n \t -----------------*****************-------------------");
+
+    return 0;
+
+
+}
