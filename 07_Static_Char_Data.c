9f6289ae81b2091b20fa0a202bffdf3ad00dab6b
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-03-30 22:31:30 +0530

    Add files via upload

---
+++ 07_Static_Char_Data.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch='P';
+    printf("\n \t Given Character is:%c",ch);
+
+    printf("\n \t ------------*******-------------");
+
+    printf("-----THANK YOU-------");
+}
