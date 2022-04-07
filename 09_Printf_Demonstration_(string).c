c1006cea3eb47d11cb053fda47571c67c59064f8
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-08 00:13:43 +0530

    Add files via upload

---
+++ 09_Printf_Demonstration_(string).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char name[8]="payal";
+
+    printf("\n \t My name is:%s",name);
+    printf("\n \t ---------*****-----------");
+
+    printf("\n \t--**thank you**----");
+
+    return 0;
+
+}
