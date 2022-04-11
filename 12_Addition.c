c82c39b1be046f584db6fb3afb74d895302a9ffa
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-11 15:21:36 +0530

    Add files via upload

---
+++ 12_Addition.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no1,no2,res=0;
+
+
+
+    printf("\n Enter first number:");
+    scanf("%d",&no1);
+
+
+
+    printf("\n Enter second number:");
+    scanf("%d",&no2);
+
+    //printf("\n--------------**********-----------------\n");
+
+    //printf("\n Your first number is:%d",no1);
+    //printf("\n Your second number is:%d",no2);
+
+    res=no1+no2;
+
+    printf("\n--------------**********-----------------\n");
+
+    printf("\n Addition is %d + %d => %d.\n",no1,no2,res);
+
+    printf("\n--------------**********-----------------\n");
+
+
+}
