13bbacbe4595d2b71ba4a7c63816ecbc4b5ac83c
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-12 21:18:03 +0530

    Add files via upload

---
+++ 13_Unformatted_Output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    printf("\n %d %s %f",255,"payal",90.20);
+
+    printf("\n %d %s %f",20,"shruti",60.500);
+
+    printf("\n %d %s %f",21,"sakshi",98.98);
+
+    getch();
+    return 0;
+}
