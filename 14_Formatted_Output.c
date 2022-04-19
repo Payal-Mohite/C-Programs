1742be96aeba85b8e84b7c4d2bb3e2c90515d4fb
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-04-19 19:48:49 +0530

    Add files via upload

---
+++ 14_Formatted_Output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n %4d|%-9s|%0.2f",255,"payal",92.91);
+
+    printf("\n %4d|%-9s|%0.2f",20,"shruti",90.90);
+
+    printf("\n %4d|%-9s|%0.3f",2,"sakshi",98.90);
+
+    getch();
+    return 0;
+}
