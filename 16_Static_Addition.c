90c57bcc503f021d5c5496af4f2889f5de413f76
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-05-06 21:50:30 +0530

    Add files via upload

---
+++ 16_Static_Addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no1=20,no2=30,res=0;
+
+    res=no1+no2;
+
+    printf("\n \t Given numbers:  %d & %d \n\n\t Addition is=   %d.",no1,no2,res);
+
+    getch();
+    return 0;
+}
