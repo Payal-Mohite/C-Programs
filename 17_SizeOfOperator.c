dd4d9c3b06f1f65c953905a989cac43db1463351
Payal Mohite <101383633+Payal-Mohite@users.noreply.github.com>
2022-05-11 21:35:49 +0530

    Add files via upload

---
+++ 17_SizeOfOperator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n Size of Integer is:    %d",sizeof(int));
+
+    printf("\n Size of Character is:  %d",sizeof(char));
+
+    printf("\n Size of Double is:     %d",sizeof(double));
+
+    printf("\n Size of Float is:      %d",sizeof(float));
+
+    printf("\n Size of Void is:       %d",sizeof(void));
+
+}
---
+++ 18_ASCII_Table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int i = 0,j = 127;
+
+    for(i = 0; i <= j; i++)
+    {
+        printf("\n Given Number %d ASCII is => %c.",i,i);
+    }
+
+    return 0;
+}
